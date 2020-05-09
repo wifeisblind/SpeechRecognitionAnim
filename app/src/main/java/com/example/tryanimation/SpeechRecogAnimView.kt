6ffae9c53319f014e35d9dd42f046c96f6f85691
Gene <gen12526099@gmail.com>
2020-05-09 18:48:40 +0800

    version 5

--- app/src/main/java/com/example/tryanimation/SpeechRecogAnimView.kt
+++ app/src/main/java/com/example/tryanimation/SpeechRecogAnimView.kt
@@ -2,15 +2,20 @@ package com.example.tryanimation
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.animation.TimeInterpolator
 import android.animation.ValueAnimator
 import android.animation.ValueAnimator.INFINITE
-import android.animation.ValueAnimator.REVERSE
 import android.content.Context
 import android.graphics.*
 import android.graphics.Color.CYAN
 import android.graphics.Paint.Style.STROKE
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import kotlin.properties.ReadOnlyProperty
+import kotlin.random.Random
+import kotlin.reflect.KProperty
 
 class SpeechRecogAnimView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
 
@@ -26,104 +31,57 @@ class SpeechRecogAnimView(context: Context, attributeSet: AttributeSet) : View(c
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         rect.set(0, 0, width, height)
         val seg = width.toFloat() / (SIZE + 1)
-        for (i in 1 until SIZE + 1) {
-            pivots.add(seg * i)
-        }
 
-        minAmp = halfHeight * 0.1f
-        offset = halfHeight * 0.8f
+        val halfHeight = height.shr(1).toFloat()
+
+        wPaths.forEachIndexed { index, wPath ->
+            wPath.init {
+                x = (index + 1) * seg
+                originY = halfHeight
+                minAmp = halfHeight * 0.1f
+                offset = when(index) {
+                    in 0..5, in 24..29 -> halfHeight * 0.1f
+                    in 6..10, in 19..23 -> halfHeight * 0.5f
+                    in 11..18 -> halfHeight * 0.8f
+                    else -> 0f
+                }
+            }
+        }
 
         super.onLayout(changed, left, top, right, bottom)
     }
 
-    private val pivots: MutableList<Float> = mutableListOf()
-
-    private var minAmp: Float = 0f
-
-    private var offset: Float = 0f
-
-    var amp1: Float = 0f
-        set(value) {
-            field = minAmp + value * offset * weight
-        }
-
-    var amp2: Float = 0f
-        set(value) {
-            field = minAmp + value * offset * weight
-        }
-
-    var amp3: Float = 0f
-        set(value) {
-            field = minAmp + value * offset * weight
-        }
-
-    var amp4: Float = 0f
-        set(value) {
-            field = minAmp + value * offset * weight
-        }
-
-    private val paths: List<Path> = List(SIZE) { Path() }
-
-    private val halfHeight: Int
-        get() = height.shr(1)
-
-    private val runtimePaths: List<Path>
-        get() {
-            return paths.mapIndexed { index, path ->
-                val x = pivots[index]
-                path.reset()
-                when(index % 4) {
-                    0 -> {
-                        path.moveTo(x, halfHeight + amp1)
-                        path.lineTo(x, halfHeight - amp1)
-                    }
-                    1 -> {
-                        path.moveTo(x, halfHeight + amp2)
-                        path.lineTo(x, halfHeight - amp2)
-                    }
-                    2 -> {
-                        path.moveTo(x, halfHeight + amp3)
-                        path.lineTo(x, halfHeight - amp3)
-                    }
-                    3 -> {
-                        path.moveTo(x, halfHeight + amp4)
-                        path.lineTo(x, halfHeight - amp4)
-                    }
-                }
-                path
+    private var wPaths: List<WPath> = List(SIZE) {
+        WPath(paint, object : ReadOnlyProperty<WPath, Float> {
+            override fun getValue(thisRef: WPath, property: KProperty<*>): Float {
+                return weight
             }
-        }
+        })
+    }
 
     override fun onDraw(canvas: Canvas) {
         canvas.save()
         canvas.drawRect(rect, paint)
-        runtimePaths.forEach {
-            canvas.drawPath(it, paint)
+
+        wPaths.forEach { path ->
+            path.draw(canvas)
         }
         canvas.restore()
     }
 
-    private val initAnim: ValueAnimator.() -> Unit = {
-        addUpdateListener {
-            invalidate()
-        }
-        repeatMode = REVERSE
-        repeatCount = INFINITE
-    }
-
-    private val factor1: ObjectAnimator = ObjectAnimator.ofFloat(this, "amp1", 0f, 0.3f, 0.2f, 1f, 0f).apply(initAnim)
-    private val factor2: ObjectAnimator = ObjectAnimator.ofFloat(this, "amp2",1f, 0.6f, 0.8f, 0.3f, 1f).apply(initAnim)
-    private val factor3: ObjectAnimator = ObjectAnimator.ofFloat(this, "amp3",0.8f, 0.4f, 0.5f, 0.2f, 0.8f).apply(initAnim)
-    private val factor4: ObjectAnimator = ObjectAnimator.ofFloat(this, "amp4",0.3f, 0.7f, 0.1f, 1f, 0.3f).apply(initAnim)
-
-    private val animSet: AnimatorSet = AnimatorSet().apply {
-        play(factor1).with(factor2).with(factor3).with(factor4)
-        duration = 1000
-    }
-
     fun startAnim() {
-        animSet.start()
+        AnimatorSet().apply {
+            val builder = play(wPaths[0].animator.also(listener))
+            for (path in wPaths.subList(1, wPaths.lastIndex)) {
+                builder.with(path.animator.apply(listener))
+            }
+            interpolator = LinearInterpolator()
+            duration = DURATION
+        }.start()
+    }
 
+    private val listener: ValueAnimator.() -> Unit = {
+        addUpdateListener { invalidate() }
     }
 
     private var weight: Float = 1f
@@ -133,6 +91,59 @@ class SpeechRecogAnimView(context: Context, attributeSet: AttributeSet) : View(c
     }
 
     companion object{
-        private const val SIZE = 20
+        private const val SIZE = 30
+        private const val DURATION = 1000L
+    }
+
+
+
+    class WPath(
+        private val paint: Paint,
+        property: ReadOnlyProperty<WPath, Float>
+    ) {
+
+        var x: Float = 0f
+        var originY: Float = 0f
+        var minAmp: Float = 0f
+        var offset: Float = 0f
+
+        private val weight: Float by property
+
+        private val path: Path = Path()
+
+        private val runtimePath: Path
+            get() = path.apply {
+                reset()
+                moveTo(x, originY + amp)
+                lineTo(x, originY - amp)
+            }
+
+        fun init(block: WPath.() -> Unit) {
+            this.apply(block)
+        }
+
+        var amp: Float = 0f
+            set(value) {
+                field = minAmp + value * offset * weight
+            }
+
+        val animator: ObjectAnimator by lazy {
+
+            val randoms = List(4) { Random.nextInt(1, 100) / 100f }
+
+            ObjectAnimator.ofFloat(this, "amp",
+                randoms[0],
+                randoms[1],
+                randoms[2],
+                randoms[3],
+                randoms[0]
+            ).apply {
+                repeatCount = INFINITE
+            }
+        }
+
+        fun draw(canvas: Canvas) {
+            canvas.drawPath(runtimePath, paint)
+        }
     }
 }
